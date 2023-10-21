a963b29c2d524a96bd182d1af7ff4556847b1af4
brensch <brendolini@protonmail.com>
2023-10-21 15:30:17 -0700

    test deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,35 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
-
+import {
+  ChakraProvider,
+  Box,
+  LinkBox,
+  LinkOverlay,
+  Image,
+  Heading,
+  Button,
+  Text,
+  Code,
+  VStack,
+} from "@chakra-ui/react"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          send is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+      <VStack spacing={6} align="center">
+        <Heading size="xl">Alyssa is cool</Heading>
+
+        <Button
+          colorScheme="blue"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          booped {count} times
+        </Button>
+        <Text mt={4}>Cool as</Text>
+      </VStack>
+    </ChakraProvider>
   )
 }
 
